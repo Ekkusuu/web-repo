68a657409c8d2f9a89c32db6f78ed096c3c0d3f7
Mio <savaluchian1@gmail.com>
2026-03-07 14:35:32 +0200

    Set base path for GitHub Pages deployment

--- lab4/astro.config.mjs
+++ lab4/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://ekkusuu.github.io',
+  base: '/web-repo/lab4',
   vite: {
     plugins: [tailwindcss()],
   },
